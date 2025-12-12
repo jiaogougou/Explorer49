410b7bc9c3928c2cf5a32f4af5b383f8666187cf
jiaogougou <jiaogougoubx@gmail.com>
2025-12-12 14:54:59 +0800

    Further add more settings to the “name” area

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+//mission button setting//
 document.addEventListener("DOMContentLoaded",function(){
     const button=document.querySelectorAll(".missionBtn");
     let openPanel = null;
@@ -31,4 +32,29 @@ document.addEventListener("DOMContentLoaded",function(){
             }
         })
     })
+    //name label setting//
+    const input = document.getElementById("playerName");
+    const btn = document.getElementById("confirmName");
+    const output = document.getElementById("saveName");
+
+    function saveName() {
+            const name = input.value.trim();
+            if(name === "") return;
+
+            output.textContent = name;
+            output.style.display = "block";
+
+            input.style.display = "none";
+            btn.style.display = "none";
+
+        }
+
+    //confirm the name//
+    btn.addEventListener("click", saveName);
+
+    input.addEventListener("keydown", function(e) {
+        if(e.key === "Enter") {
+            saveName();
+        }
+    });
 })
