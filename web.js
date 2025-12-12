b3e1cef4d3b5f76545b417ad966fcf91d4ee614d
jiaogougou <jiaogougoubx@gmail.com>
2025-12-12 11:15:53 +0800

    keep updating content pages

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
     const button=document.querySelectorAll(".missionBtn");
     let openPanel = null;
+    let activeBtn = null
 
     button.forEach(button =>{
         button.addEventListener("click", function(){
@@ -13,13 +14,20 @@ document.addEventListener("DOMContentLoaded",function(){
                 openPanel.classList.remove("show");
         
             }
+            if (activeBtn && activeBtn !== this){
+                activeBtn.classList.remove("active");
+            }
 
             if(panel.classList.contains("show")){
                 panel.classList.remove("show");
+                this.classList.remove("active");
                 openPanel=null;
+                activeBtn=null;
             }else {
                 panel.classList.add("show");
+                this.classList.add("active");
                 openPanel=panel
+                activeBtn= this
             }
         })
     })
